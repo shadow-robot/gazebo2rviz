9c4fc509ee43c1ee41b26983b85ac5918bb8df3a
Fotios <fotios@shadowrobot.com>
2021-12-23 10:46:17 +0000

    fixing gazebo2rviz new msg type

--- src/gazebo2rviz/sdf2moveit_collision_node.py
+++ src/gazebo2rviz/sdf2moveit_collision_node.py
@@ -264,7 +264,6 @@ class Sdf2moveit(object):
                         planning_scene_msg.world.collision_objects[-1].pose = collision_object.primitive_poses[0]
                     elif(len(collision_object.mesh_poses)>0):
                         planning_scene_msg.world.collision_objects[-1].pose = collision_object.mesh_poses[0]
-        print(planning_scene_msg)
         self.planning_scene_pub.publish(planning_scene_msg)
 
 
